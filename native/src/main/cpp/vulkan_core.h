29ac737a1292c6c01dfd2b44c4df843a97c40e79
Twicefear <ytd82774@gmail.com>
2026-05-12 23:24:48 +0530

    Add optimization flags and function declarations
    Added feature flags and function declarations for optimizations.

--- native/src/main/cpp/vulkan_core.h
+++ native/src/main/cpp/vulkan_core.h
@@ -18,6 +18,21 @@ void setIndirectDrawEnabled(bool enabled);
 void setMultiThreadedRendering(bool enabled);
 void setTargetFPS(int fps);
 
+// Add these feature flags at the top, after the includes
+extern bool g_optimizeEntityRendering;
+extern bool g_optimizeBlockPlacement;
+extern bool g_optimizeHitDetection;
+extern bool g_optimizeCameraMovement;
+extern bool g_fixReplayLag;
+extern bool g_highPerformanceMode;
+
+// Add these function declarations
+extern "C" void setOptimizationFlags(bool entities, bool blocks, bool hits, bool camera, bool replay, bool highPerf);
+extern "C" void applyRealtimeOptimizations();
+extern "C" void onBlockPlaceEvent();
+extern "C" void onHitEvent();
+extern "C" void onCameraMove(float deltaX, float deltaY);
+
 #ifdef __cplusplus
 }
 #endif
