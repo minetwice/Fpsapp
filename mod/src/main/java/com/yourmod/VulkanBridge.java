c4604c10bbc8870ce934d22794a0d93c673b5788
Twicefear <ytd82774@gmail.com>
2026-05-13 06:10:07 +0530

    Update VulkanBridge.java

--- mod/src/main/java/com/yourmod/VulkanBridge.java
+++ mod/src/main/java/com/yourmod/VulkanBridge.java
@@ -5,10 +5,16 @@ public class VulkanBridge {
         System.loadLibrary("vulkan_renderer");
     }
 
+    // Core methods
     public static native boolean nativeInitVulkan(Object surface);
     public static native void nativeRenderFrame();
     public static native void nativeCleanup();
-    public static native void setIndirectDrawEnabled(boolean enabled);
-    public static native void setMultiThreadedRendering(boolean enabled);
+
+    // Optimization methods
+    public static native void setOptimizationFlags(boolean entities, boolean blocks, boolean hits, boolean camera, boolean replay, boolean highPerf);
     public static native void setTargetFPS(int fps);
+    public static native void applyRealtimeOptimizations();
+    public static native void onBlockPlaceEvent();
+    public static native void onHitEvent();
+    public static native void onCameraMove(float deltaX, float deltaY);
 }
