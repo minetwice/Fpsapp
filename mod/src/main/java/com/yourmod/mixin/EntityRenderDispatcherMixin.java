1097604c8f9b09808a8ec1b2885d1d8b5b700576
Twicefear <ytd82774@gmail.com>
2026-05-13 14:44:51 +0530

    Update EntityRenderDispatcherMixin.java

--- mod/src/main/java/com/yourmod/mixin/EntityRenderDispatcherMixin.java
+++ mod/src/main/java/com/yourmod/mixin/EntityRenderDispatcherMixin.java
@@ -1,22 +1,27 @@
 package com.yourmod.mixin;
 
 import com.yourmod.PerformanceMonitor;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.joml.Matrix4f;
 
 @Mixin(EntityRenderDispatcher.class)
-public class EntityRenderDispatcherMixin {
-    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
-    private void onShouldRender(Entity entity, Vec3d cameraPos, double range, CallbackInfoReturnable<Boolean> cir) {
-        double distance = entity.squaredDistanceTo(cameraPos.x, cameraPos.y, cameraPos.z);
+public abstract class EntityRenderDispatcherMixin {
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private <E extends Entity> void onRender(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        double distance = Math.sqrt(x * x + y * y + z * z);
         double maxDist = PerformanceMonitor.getEntityCullingDistance();
-        if (distance > maxDist * maxDist) {
-            cir.setReturnValue(false);
+        if (distance > maxDist) {
+            ci.cancel();
         }
     }
 }
