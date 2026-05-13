f5dae52534b467c983ee2e5d5b0e28305b0fe10d
Twicefear <ytd82774@gmail.com>
2026-05-13 07:51:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/yourapp/MainActivity.java
+++ app/src/main/java/com/yourapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
