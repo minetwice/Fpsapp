915375546d7d302940e9f129e7aba627f564ecc3
Twicefear <ytd82774@gmail.com>
2026-05-14 21:59:39 +0530

    Update SetupActivity.kt

--- app/src/main/java/com/fearlauncher/SetupActivity.kt
+++ app/src/main/java/com/fearlauncher/SetupActivity.kt
@@ -7,20 +7,11 @@ import androidx.activity.compose.setContent
 import com.fearlauncher.ui.theme.RedBlackTheme
 
 class SetupActivity : ComponentActivity() {
-
-    // Using top-level Component class from SetupScreen.kt
-    private val requiredComponents = listOf(
-        Component("Java Runtime (JRE) 17", "https://github.com/AdoptOpenJDK/openjdk17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jre_aarch64_linux_hotspot_17.0.2_8.tar.gz", "jre17.tar.gz", 80_000_000),
-        Component("Minecraft Libraries", "https://launcher.mojang.com/v1/objects/.../libraries.zip", "libraries.zip", 150_000_000),
-        Component("LWJGL 3.3.6", "https://build.lwjgl.org/release/3.3.6/lwjgl-3.3.6.zip", "lwjgl.zip", 30_000_000)
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RedBlackTheme {
                 SetupScreen(
-                    components = requiredComponents,
                     onAllComponentsReady = {
                         startActivity(Intent(this, MainActivity::class.java))
                         finish()
