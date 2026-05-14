da91674433ec2780edba9110117a24f17ee0c28a
Twicefear <ytd82774@gmail.com>
2026-05-14 22:17:03 +0530

    Update MainActivity.kt

--- app/src/main/java/com/fearlauncher/MainActivity.kt
+++ app/src/main/java/com/fearlauncher/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.fearlauncher
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).text = "Fear Launcher Main Activity"
     }
 }
